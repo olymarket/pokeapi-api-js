a53327a169af854a14c662dd505eb22128fd89ce
olymarket <olymarkeet@gmail.com>
2025-03-22 00:52:21 -0600

    1.1- consuming the Pokémon API

--- api/admin/blog-index.js
+++ api/admin/blog-index.js
@@ -1,1 +1,45 @@
-console.log('caca');
+document.addEventListener("DOMContentLoaded", () => {
+    pokeloading();
+
+    document.getElementById("pokeprev").addEventListener("click", pokeprev);
+    document.getElementById("pokenext").addEventListener("click", pokenext);
+});
+
+let   offset = 0;
+const limit  = 10;
+
+function pokeloading(){
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    fetch(url)
+    .then(response => response.json())
+    .then (data =>{
+        const list_post = document.getElementById('list_post').querySelector('tbody');
+        list_post.innerHTML = '';
+        data.results.forEach(post => {
+            const id = post.url.split('/').slice(-2, -1)[0];
+    
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td># ${id}</td>
+                <td>${post.name}</td>
+                <td>${post.url}</td>
+            `;
+            list_post.appendChild(tr);
+        });
+    })
+    .catch(error =>{
+        console.log('Error fetch:', error);
+    });
+}
+
+function pokeprev(){
+    if(offset > 0){
+        offset -= limit;
+        pokeloading();
+    }
+}
+
+function pokenext(){
+    offset += limit;
+    pokeloading();
+}
